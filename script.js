8396e202639ad09ec01c8adf5d8f78d1e1f54f76
Lexius Waltar <lexius.waltar@gmail.com>
2025-02-24 19:02:18 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+// 1. Create a function called changeText that changes the text inside the <h1> tag when called.
+// Add this function to the onclick attribute of a button in index.html: <button onclick="changeText()">Change Text</button>
+
+
+// 2. Create a function called changeColor that changes the background color of the page when a button is clicked.
+// Add this function to the onclick attribute of a button in index.html: <button onclick="changeColor()">Change Background Color</button>
+
+
+// 3. Create a function called toggleImage that toggles the visibility of the image when a button is clicked.
+// Add this function to the onclick attribute of a button in index.html: <button onclick="toggleImage()">Toggle Image</button>
+
+
+// 4. Create a function called growBox that increases the size of the div box when a button is clicked.
+// Add this function to the onclick attribute of a button in index.html: <button onclick="growBox()">Grow Box</button>
+
+
+// 5. Create a function called changeParagraph that changes the text inside the paragraph when a button is clicked.
+// Add this function to the onclick attribute of a button in index.html: <button onclick="changeParagraph()">Change Paragraph</button>
+
+
+// 6. Create a function called changeTextColor that changes the text color of the heading when a button is clicked.
+// Add this function to the onclick attribute of a button in index.html: <button onclick="changeTextColor()">Change Text Color</button>
+
+
+// 7. Create a function called hideParagraph that hides the paragraph when a button is clicked.
+// Add this function to the onclick attribute of a button in index.html: <button onclick="hideParagraph()">Hide Paragraph</button>
+
+
+// 8. Create a function called changeLink that changes the link URL to "https://www.google.com" and updates its text to "Go to Google" when a button is clicked.
+// Add this function to the onclick attribute of a button in index.html: <button onclick="changeLink()">Change Link</button>
+
+
+// 9. Create a function called changeImage that changes the image source to "newimage.jpg" when a button is clicked.
+// Add this function to the onclick attribute of a button in index.html: <button onclick="changeImage()">Change Image</button>
+
+
+// 10. Create a function called addBorder that adds a black border to the div box when a button is clicked.
+// Add this function to the onclick attribute of a button in index.html: <button onclick="addBorder()">Add Border</button>
